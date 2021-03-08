540ae7c35706c6c6b11fd61c2212abed812c91fe
Matthew <matthew.d.baker20@gmail.com>
2021-03-08 13:33:50 -0600

    first commit

---
+++ src/main/java/com/podlobby/podlobby/PodlobbyApplication.java
@@ -0,0 +1,13 @@
+package com.podlobby.podlobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodlobbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodlobbyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/podlobby/podlobby/PodlobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podlobby.podlobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodlobbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
