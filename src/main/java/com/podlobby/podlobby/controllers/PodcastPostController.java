dcac79e5e56a878fa9e0c032c8556e5119d4431b
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-03-09 09:46:44 -0600

    placed navbar in edit and create posts

--- src/main/java/com/podlobby/podlobby/controllers/PodcastPostController.java
+++ src/main/java/com/podlobby/podlobby/controllers/PodcastPostController.java
@@ -3,20 +3,37 @@ package com.podlobby.podlobby.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class PodcastPostController {
 
-
+    /////////////////
+    //    DISPLAY  //
+    /////////////////
     @GetMapping("/posts/create")
         public String showPostCreate(){
             return"/posts/create";
         }
 
-    //            Will need to add ID to path for specific post
+
+    /////////////////
+    //EDIT   OPTION//
+    /////////////////
+    //Will need to add ID to path for specific post
     @GetMapping("/posts/edit")
     public String viewEditPostForm() {
 //        model.addAttribute("post", postsDao.getOne(id));
         return "posts/edit";
     }
+
+    /////////////////
+    //DELETE OPTION//
+    /////////////////
+//    @PostMapping("/posts/{id}/delete")
+//    public String deletePost(@PathVariable long id){
+//        System.out.println("Deleting post...");
+//        postsDao.deleteById(id);
+//        return "redirect:/posts";
+//    }
 
 }
