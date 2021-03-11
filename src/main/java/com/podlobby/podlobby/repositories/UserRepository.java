e82b835659f257e92c616ff1712439fff0b404ce
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-03-11 16:42:02 -0600

    register user logic and db access

--- src/main/java/com/podlobby/podlobby/repositories/UserRepository.java
+++ src/main/java/com/podlobby/podlobby/repositories/UserRepository.java
@@ -4,8 +4,6 @@ import com.podlobby.podlobby.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
-
-
+    User findByUsername(String username);
+    User findByEmail(String email);
 }
