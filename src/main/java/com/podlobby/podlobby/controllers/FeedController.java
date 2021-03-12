f7aca4896d51a6f633ba2aa8719d6d715b6deec1
Matthew <matthew.d.baker20@gmail.com>
2021-03-12 13:26:35 -0600

    added i frame parser for now to add styling to iframes on feed

--- src/main/java/com/podlobby/podlobby/controllers/FeedController.java
+++ src/main/java/com/podlobby/podlobby/controllers/FeedController.java
@@ -3,16 +3,19 @@ package com.podlobby.podlobby.controllers;
 import com.podlobby.podlobby.model.Podcast;
 import com.podlobby.podlobby.repositories.PodcastRepository;
 import com.podlobby.podlobby.repositories.UserRepository;
+import com.podlobby.podlobby.util.IframeParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class FeedController {
     private final PodcastRepository podcastDao;
     private final UserRepository usersDao;
+    private final IframeParser iframeParser = new IframeParser();
 
     public FeedController(PodcastRepository podcastDao, UserRepository usersDao ){
         this.podcastDao = podcastDao;
@@ -22,6 +25,10 @@ public class FeedController {
     @GetMapping("/feeds/global")
     public String showGlobalFeed(Model model){
         List<Podcast> podcasts = podcastDao.findAll();
+        for(Podcast p : podcasts){
+            String parsedEmbed = iframeParser.parseIframe(p.getEmbedLink());
+            p.setEmbedLink(parsedEmbed);
+        }
         model.addAttribute("podcasts", podcasts);
         return"/feeds/global-feed";
     }
