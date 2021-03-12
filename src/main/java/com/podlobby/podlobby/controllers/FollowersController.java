8ea1b67d9b726102621fa68c6039d0760a323915
Matthew <matthew.d.baker20@gmail.com>
2021-03-12 10:44:55 -0600

    spring security added, logout logic, authorized links, allowed pages

--- src/main/java/com/podlobby/podlobby/controllers/FollowersController.java
+++ src/main/java/com/podlobby/podlobby/controllers/FollowersController.java
@@ -1,15 +1,36 @@
 package com.podlobby.podlobby.controllers;
 
+import com.podlobby.podlobby.repositories.FollowRepository;
+import com.podlobby.podlobby.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FollowersController {
 
+    private final FollowRepository followDao;
+    private final UserRepository userDao;
+
+    public FollowersController(FollowRepository followDao, UserRepository userDao){
+        this.followDao = followDao;
+        this.userDao = userDao;
+    }
+
     //Will need to ad ID for specific followers on page
     @GetMapping("/followers")
-    public String showFollowers(){
+    public String showFollowers(Model model){
+        model.addAttribute("followList", followDao.findAllByUserId(1L));
         return"followers";
+    }
+
+
+    @GetMapping("/followers/{id}")
+    public String viewFollowersProfile(Model model, @PathVariable(name = "id") long id){
+        model.addAttribute("follower", userDao.findById(id)); // maybe make a method in the userDao that only selects public information?
+        return "followers";
     }
 
 }
---
+++ src/main/java/com/podlobby/podlobby/services/UserDetailsLoader.java
@@ -0,0 +1,31 @@
+package com.podlobby.podlobby.services;
+
+import com.podlobby.podlobby.model.User;
+import com.podlobby.podlobby.model.UserWithRoles;
+import com.podlobby.podlobby.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsLoader implements UserDetailsService {
+
+    private final UserRepository userDao;
+
+    public UserDetailsLoader(UserRepository userDao){
+        this.userDao = userDao;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("find a user with UN : " + username);
+        User user = userDao.findByUsername(username);
+        System.out.println("----before null check, is there a user? ----");
+        if(user == null){
+            throw new UsernameNotFoundException("No user found with username " + username);
+        }
+        System.out.println(user.getUsername());
+        return new UserWithRoles(user); // the enhanced UserDetails copy user
+    }
+}
